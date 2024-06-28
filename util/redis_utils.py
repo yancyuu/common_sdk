ff32b8d82942134d0909e00d83b84ba3cba9bd8a
yancyyu <1191500820@qq.com>
2024-06-28 13:57:34 +0800

    refactor(session): 删除处理大模型session的逻辑
    优化Redis模块，将异步和同步操作分开

--- util/redis_utils.py
+++ util/redis_utils.py
@@ -1,137 +1,154 @@
 # -*- coding: utf-8 -*-
 
 import json
+import time
 from common_sdk.base_class.singleton import SingletonMetaThreadSafe
 import redis
 import aioredis
+import aioredlock
+from typing import TypeVar, Generic, Union
 from ..system import sys_env
 
+T = TypeVar('T')
 
-class RedisStorage(metaclass=SingletonMetaThreadSafe):
-
-
+class BaseRedisStorage(Generic[T], metaclass=SingletonMetaThreadSafe):
+    
     def __init__(self) -> None:
-        self._redis = redis.Redis.from_url(url=sys_env.get_env('REDIS_ADDRESS'))
-        self._redis_async = None
+        self.redis_addresses = sys_env.get_env('REDIS_ADDRESSES').split(',')
+        self.master_address = self.redis_addresses[0]
+        self._redis = redis.Redis.from_url(self.master_address)
 
-    def set(self, key, data, expired=7200) -> bool:
+        self._redlock = aioredlock.Aioredlock([{"address": url} for url in self.redis_addresses])
+        self._redis_async_client = None
+
+    def set(self, key: str, data: T, expired: int = 7200) -> bool:
         if not key:
             raise Exception("找不到key")
         return self._redis.setex(key, expired, json.dumps(data))
 
-    def get(self, key) -> dict:
+    def get(self, key: str) -> Union[T, None]:
         if not key:
             raise Exception("找不到key")
         data = self._redis.get(key)
         if not data:
-            return {}
+            return None
         return json.loads(data)
 
-    def check(self, key, expired=24 * 60 * 60) -> bool:
+    def check(self, key: str, expired: int = 24 * 60 * 60) -> bool:
         if not key:
             raise Exception("找不到key")
         return self._redis.expire(key, expired)
 
-    def delete(self, key) -> None:
+    def delete(self, key: str) -> None:
         if not key:
             raise Exception("找不到key")
         self._redis.delete(key)
     
-    def enqueue_message(self, key, message) -> None:
-        """将消息添加到队列中"""
+    def enqueue_message(self, key: str, message: T) -> None:
         self._redis.lpush(key, json.dumps(message))
     
-    def dequeue_message(self, key, timeout=0) -> dict:
-        """从队列中移除并返回一条消息"""
+    def dequeue_message(self, key: str, timeout: int = 0) -> Union[T, None]:
         message = self._redis.brpop(key, timeout=timeout)
         if message:
             return json.loads(message[1])
         return None
-
-    def acquire_lock(self, lock_name, lock_timeout=60):
-        """尝试获取锁，成功则返回 True，否则返回 False。"""
-        result = self._redis.set(lock_name, 1, ex=lock_timeout, nx=True)
-        return result is not None
-
-    def release_lock(self, lock_name):
-        """释放锁。"""
-        self._redis.delete(lock_name)
     
-    async def acquire_lock_async(self, lock_name, lock_timeout=60):
-        await self._ensure_async_client()
-        if lock_timeout == -1:
-            # 当不希望键过期时，省略 EX 参数
-            result = await self._redis_async.set(lock_name, 1, nx=True)
-        else:
-            # 确保过期时间是一个正整数
-            result = await self._redis_async.set(lock_name, 1, ex=max(1, int(lock_timeout)), nx=True)
-        return result is not None
+    def acquire_lock(self, lock_name: str, lock_timeout: int = 60):
+        start_time = time.time() * 1000
+        lock = self._redlock.lock(lock_name, lock_timeout * 1000)
+        elapsed_time = time.time() * 1000 - start_time
 
+        if not lock:
+            return False
 
-    async def release_lock_async(self, lock_name):
-        """释放锁。"""
-        await self._ensure_async_client()
-        await self._redis_async.delete(lock_name)
-        
-    async def _ensure_async_client(self):
-        if not self._redis_async:
-            self._redis_async = aioredis.from_url(
-                url=sys_env.get_env('REDIS_ADDRESS'),
+        adjusted_lock_timeout = lock_timeout * 1000 - elapsed_time
+        if adjusted_lock_timeout <= 0:
+            self._redlock.unlock(lock)
+            return False
+
+        return lock
+
+    def release_lock(self, lock) -> None:
+        self._redlock.unlock(lock)
+
+class AsyncRedisStorage(BaseRedisStorage[T]):
+
+    @property
+    async def redis_async_client(self):
+        if not self._redis_async_client:
+            self._redis_async_client = await aioredis.create_redis_pool(
+                self.master_address,
                 encoding="utf-8",
                 decode_responses=True
             )
+        return self._redis_async_client
     
-    async def set_async(self, key, data, expired=None) -> bool:
-        await self._ensure_async_client()
+    async def set(self, key: str, data: T, expired: int = None) -> bool:
+        redis_async_client = await self.redis_async_client
         if not key:
             raise Exception("找不到key")
         if expired is None:
-            # 没有过期时间，使用set命令
-            return await self._redis_async.set(key, json.dumps(data))
+            return await redis_async_client.set(key, json.dumps(data))
         else:
-            # 有过期时间，使用setex命令
-            return await self._redis_async.setex(key, expired, json.dumps(data))
+            return await redis_async_client.setex(key, expired, json.dumps(data))
 
-    async def get_async(self, key):
-        await self._ensure_async_client()
+    async def get(self, key: str) -> Union[T, None]:
+        redis_async_client = await self.redis_async_client
         if not key:
             raise Exception("找不到key")
-        data = await self._redis_async.get(key)
+        data = await redis_async_client.get(key)
         return json.loads(data) if data else None
 
-    async def check_async(self, key, expired=24 * 60 * 60) -> bool:
-        await self._ensure_async_client()
+    async def check(self, key: str, expired: int = 24 * 60 * 60) -> bool:
+        redis_async_client = await self.redis_async_client
         if not key:
             raise Exception("找不到key")
-        return await self._redis_async.expire(key, expired)
+        return await redis_async_client.expire(key, expired)
 
-    async def delete_async(self, key) -> None:
-        await self._ensure_async_client()
+    async def delete(self, key: str) -> None:
+        redis_async_client = await self.redis_async_client
         if not key:
             raise Exception("找不到key")
-        await self._redis_async.delete(key)
+        await redis_async_client.delete(key)
 
-    async def close_async(self):
-        if self._redis_async:
-            self._redis_async.close()
-            await self._redis_async.wait_closed()
-    
-    async def enqueue_message_async(self, queue_name, message) -> None:
-        """异步将消息添加到队列中"""
-        await self._ensure_async_client()
-        await self._redis_async.lpush(queue_name, json.dumps(message))
+    async def close(self) -> None:
+        if self._redis_async_client:
+            self._redis_async_client.close()
+            await self._redis_async_client.wait_closed()
 
-    async def dequeue_message_async(self, queue_name, timeout=0) -> dict:
-        """异步从队列中移除并返回一条消息"""
-        await self._ensure_async_client()
-        message = await self._redis_async.brpop(queue_name, timeout=timeout)
+    async def enqueue_message(self, queue_name: str, message: T) -> None:
+        redis_async_client = await self.redis_async_client
+        await redis_async_client.lpush(queue_name, json.dumps(message))
+
+    async def dequeue_message(self, queue_name: str, timeout: int = 0) -> Union[T, None]:
+        redis_async_client = await self.redis_async_client
+        message = await redis_async_client.brpop(queue_name, timeout=timeout)
         if message:
             return json.loads(message[1])
         return None
 
-    async def get_queue_length_async(self, queue_name):
-        """异步获取队列长度"""
-        await self._ensure_async_client()
-        return await self._redis_async.llen(queue_name)
-    
-redis_storage = RedisStorage()
+    async def get_queue_length(self, queue_name: str) -> int:
+        redis_async_client = await self.redis_async_client
+        return await redis_async_client.llen(queue_name)
+
+    async def acquire_lock(self, lock_name: str, lock_timeout: int = 60):
+        start_time = time.time() * 1000
+        lock = await self._redlock.lock(lock_name, lock_timeout)
+        elapsed_time = time.time() * 1000 - start_time
+
+        if not lock:
+            return False
+
+        adjusted_lock_timeout = lock_timeout * 1000 - elapsed_time
+        if adjusted_lock_timeout <= 0:
+            await self._redlock.unlock(lock)
+            return False
+
+        return lock
+
+    async def release_lock(self, lock) -> None:
+        await self._redlock.unlock(lock)
+
+# 示例使用
+redis_storage = BaseRedisStorage[dict]()
+async_redis_storage = AsyncRedisStorage[dict]()
