a8d1fa1800d74459f28a9512e6f51d44e0318b82
yancyyu <yancyyu.ok@gmail.com>
2024-08-12 15:28:07 +0800

    修复(enum): 解决枚举字段输出非字符串的问题
    确保枚举字段正确输出为字符串，以防止类型相关的问题。

--- data_transform/protobuf_transformer.py
+++ data_transform/protobuf_transformer.py
@@ -25,11 +25,30 @@ def batch_dict_to_protobuf(protobuf_json_list, protobuf_cls):
 def protobuf_to_dict(protobuf):
     if protobuf is None:
         return None
-    dict = json_format.MessageToDict(
+
+    # 使用 json_format 将 Protobuf 对象转换为字典
+    dict_result = json_format.MessageToDict(
         protobuf,
         preserving_proto_field_name=True,  # 保持字段名与 Protobuf 定义的一致
-        use_integers_for_enums=False)  # 使用枚举的名称而不是整数值
-    return dict
+        use_integers_for_enums=False  # 将枚举值转换为整数
+    )
+
+    # 手动检查并确保枚举字段转换为名称字符串
+    for field in protobuf.DESCRIPTOR.fields:
+        field_name = field.name
+        if field_name not in dict_result:
+            # 检查字段是否是枚举类型
+            if field.enum_type is not None:
+                # 获取字段的枚举名称
+                enum_value = getattr(protobuf, field_name)
+                enum_name = field.enum_type.values_by_number[enum_value].name
+                dict_result[field_name] = enum_name
+            else:
+                # 获取默认值并添加到字典
+                default_value = getattr(protobuf, field_name)
+                dict_result[field_name] = default_value
+
+    return dict_result
 
 
 def batch_protobuf_to_dict(protobuf_list):
